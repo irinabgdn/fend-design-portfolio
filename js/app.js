0147efe9ce4a0cca075e7edb13c396fe93f69144
Irina Bogdan <irina.bogdan1@gmail.com>
2018-07-18 18:56:40 +0300

    Menu and info pages

--- js/app.js
+++ js/app.js
@@ -1,38 +1,69 @@
 var openclose;
+var showHome;
+var showProjects;
+var showAbout;
+var showContact;
+
+let menuButton = $(".close-button");
 
 openclose = function() {
   return $(".close-button").click(function() {
-    $(".nav-bar").toggleClass("open");
+    if ($(".info-page").hasClass("open")) {
+      $(".info-page").removeClass("open");
+    } else {
+      $(".nav-bar").toggleClass("open");
+    };
     return $(this).toggleClass("open");
   });
 };
 
-jQuery(document).ready(function($) {
-  return openclose();
+function closeNav() {
+  return $(".nav-bar").removeClass("open");
+};
+
+function closeMenu() {
+  $(".close-button").removeClass("open");
+};
+
+function openMenuBtn() {
+  if (!$(".close-button").hasClass("open")) {
+    $(".close-button").toggleClass("open");
+  };
+}
+
+showHome = function() {
+  $(".home-btn").click(function() {
+    window.location.reload(true);
+  });
+};
+
+showProjects = function() {
+  $(".projects-btn").click(function() {
+    closeNav();
+    closeMenu();
+  });
+};
+
+showAbout = function() {
+  $(".about-btn").click(function(){
+    openMenuBtn();
+    closeNav();
+    $("#about").addClass("open");
+  })
+}
+
+showContact = function() {
+  return $(".contact-btn").click(function(){
+    openMenuBtn();
+    closeNav();
+    $("#contact").addClass("open");
+  })
+}
+
+jQuery(document).ready(function() {
+  openclose();
+  showAbout();
+  showHome();
+  showProjects();
+  showContact();
 });
-
-// $(document).ready(function(){
-$('#prj-container').on('click', '.project', function(e) {
-        e.preventDefault();
-        debugger
-
-        $(this).toggleClass('prj-open');
-        } )
-
-// // Navigation freezes when scrolled down
-
-// let navTop = $('nav').offset().top;
-// $(window).scroll(function() {
-//     let currentScroll = $(window).scrollTop();
-//     if (currentScroll >= navTop) {
-//         $('nav').classList.toggle("on-top")
-//         // $('nav').css({
-//         //     position: 'fixed',
-//         //     top: '0',
-//         //     transform: 'translateY(-200px)'
-//         // });
-//     } else {
-//         $('nav').removeClass("on-top")
-//         };
-//     })
-// })
